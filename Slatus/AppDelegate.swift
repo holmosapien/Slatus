2ef7f86d9365444080c10fd6d4f39d54e0a4425a
Dan Holm <dan@macrofab.net>
2019-04-06 22:50:02 -0500

    Initial Commit

---
+++ Slatus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Slatus
+//
+//  Created by Dan Holm on 4/6/19.
+//  Copyright © 2019 Holmosapien. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
